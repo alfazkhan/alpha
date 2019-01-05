994be8e64c09ede528e4f18b4f530276abc24a65
Alfaz Khan <khanalfaaz14@gmail.com>
2019-01-05 23:29:52 +0530

    Created Model,table and added validations to it

--- app/models/bookings.rb
+++ app/models/bookings.rb
@@ -1,3 +1,6 @@
 class Bookings < ApplicationRecord
 
+validates :Movie , presence:true, length: {minimum: 30 , maximum: 30}
+validates :Description , presence:true, length: {minimum: 30, maximum: 300 }
+
 end
