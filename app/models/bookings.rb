6d4c0061d5a1d2d96df4a9049a8322e54b8607c3
Alfaz Khan <khanalfaaz14@gmail.com>
2019-01-08 18:23:18 +0530

    Styling to New and Edit

--- app/models/bookings.rb
+++ app/models/bookings.rb
@@ -1,7 +1,7 @@
 class Bookings < ApplicationRecord
 
 validates :Movie , presence:true, length: {minimum: 3 , maximum: 30},uniqueness: true
-validates :Description , presence:true, length: {minimum: 3, maximum: 300 },uniqueness: true
+validates :Description , presence:true, length: {minimum: 3, maximum: 3000 },uniqueness: true
 validates :Rating , presence:true, length: {minimum: 0, maximum: 100 }
 
 
