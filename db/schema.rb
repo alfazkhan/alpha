50bf79301138cc9a9aa8faf79015b37c1977d958
Alfaz Khan <khanalfaaz14@gmail.com>
2019-01-11 22:35:47 +0530

    Deleted Users table, not working

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_01_10_182857) do
+ActiveRecord::Schema.define(version: 2019_01_11_072040) do
 
   create_table "bookings", options: "ENGINE=InnoDB DEFAULT CHARSET=latin1", force: :cascade do |t|
     t.string "Movie"
@@ -18,12 +18,6 @@ ActiveRecord::Schema.define(version: 2019_01_10_182857) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer "Rating"
-  end
-
-  create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=latin1", force: :cascade do |t|
-    t.string "username"
-    t.string "email"
-    t.integer "user_id"
   end
 
 end
