1709db1cbf1450c9f1bb406127f58befea0f4158
Alfaz Khan <khanalfaaz14@gmail.com>
2019-01-12 14:33:06 +0530

    Added Password Mechanism

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_01_11_180702) do
+ActiveRecord::Schema.define(version: 2019_01_12_085813) do
 
   create_table "bookings", options: "ENGINE=InnoDB DEFAULT CHARSET=latin1", force: :cascade do |t|
     t.string "Movie"
@@ -26,6 +26,7 @@ ActiveRecord::Schema.define(version: 2019_01_11_180702) do
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password_digest"
   end
 
 end
